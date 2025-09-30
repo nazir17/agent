4c2ba0bdd39173f71c829ddf1817822b6fe64382
nazir17 <naziruddin@tothebyte.com>
2025-09-30 20:09:49 +0530

    created an agent to give streaming response

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-import os
-from fastapi import FastAPI
-from pydantic import BaseModel
-from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+
 
 
 load_dotenv()
 
+
 app = FastAPI()
 
-class PromptRequest(BaseModel):
-    prompt: str
 
-@app.post("/example")
-async def generate_text(req: PromptRequest):
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
-        google_api_key=os.getenv("GENAI_API_KEY"),
-        temperature=0.7
-    )
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key_api_key=os.environ["GOOGLE_API_KEY"],
+    streaming=True,
+    temperature=0.7
+)
 
-    response = llm.invoke(req.prompt)
-    return {"response": response.content}
+
+prompt = ChatPromptTemplate.from_template("You are an AI agent. Answer the question:{input}")
+
+
+async def generate_stream(prompt_text: str):
+    chain = prompt | llm
+
+    async for token in chain.astream({"input": prompt_text}):
+        if token.content:
+            yield token.content
+
+
+@app.get("/stream")
+async def stream_endpoint(request: Request, q: str):
+
+    async def event_generator():
+        async for token in generate_stream(q):
+            if await request.is_disconnected():
+                break
+            yield f"data: {token}"
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
